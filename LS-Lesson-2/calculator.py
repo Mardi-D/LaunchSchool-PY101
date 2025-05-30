30242c64e585d72eeb570945de0b325f6b7eef8e
mardi_d <mduckworth@gmail.com>
2025-05-30 14:25:53 -0700

    Updated program with  while loop and helper functions

--- LS-Lesson-2/calculator.py
+++ LS-Lesson-2/calculator.py
@@ -4,24 +4,50 @@
 # Perform the operation on the two numbers.
 # Print the result to the terminal.
 
+# Helper functions
 def prompt(message):
     print(f'==> {message}')
 
 
+def invalid_number(number_str):
+    try:
+        int(number_str)
+    except ValueError:
+        return True
+
+    return False
+
+
 prompt('Welcome to Calculator!')
 
-# Ask the user for the first number.
-prompt('What is the first number?')
+prompt('What is the first number?')  # Ask the user for the first number.
 number1 = input()
+
+# Confirm user provided valid input for first number.
+while invalid_number(number1):
+    prompt('Hmmm... That does not look like a valid number.')
+    number1 = input()
+
 # Ask the user for the second number.
 prompt('What is the second number?')
 number2 = input()
+
+# Confirm user provided valid input for second number.
+while invalid_number(number2):
+    prompt('Hmmm... That does not look like a valid number.')
+    number2 = input()
 
 # print(f'{number1} {number2}')
 
 # Perform the operation on the two numbers.
 prompt('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide')
 operation = input()
+
+# Validate the operation requested by the user.
+while operation not in ["1", "2", "3", "4"]:
+    # ask again
+    prompt('You must choose 1, 2, 3, or 4')
+    operation = input()
 
 # if operation == '1':  # add
 #     output = int(number1) + int(number2)
