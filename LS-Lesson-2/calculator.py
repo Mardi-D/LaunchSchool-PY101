2b4cb2be9db750222d2270a23cf19d1a2742d3c0
mardi_d <mduckworth@gmail.com>
2025-06-12 15:42:29 -0700

    Resaving to Calculator program'
    git status
    quit
    exit

--- LS-Lesson-2/calculator.py
+++ LS-Lesson-2/calculator.py
@@ -1,8 +1,10 @@
+
 # Ask the user for the first number.
 # Ask the user for the second number.
 # Ask the user for an operation to perform.
 # Perform the operation on the two numbers.
 # Print the result to the terminal.
+# Ask the user if they want to perform another calculation.
 
 # Helper functions
 def prompt(message):
@@ -48,6 +50,7 @@ while operation not in ["1", "2", "3", "4"]:
     # ask again
     prompt('You must choose 1, 2, 3, or 4')
     operation = input()
+
 
 # if operation == '1':  # add
 #     output = int(number1) + int(number2)
